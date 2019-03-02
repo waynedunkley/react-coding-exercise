a43bac34c831d22af7cf8d0c64abe25f9739fffb
Wayne Dunkley <waynedunkley@Waynes-MacBook-Pro.local>
2019-03-02 13:56:29 +1100

    create favourites store

---
+++ src/reducers/favourites.js
@@ -0,0 +1,13 @@
+const initialState = {
+  busy: false,
+  favourites: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import config from './config'
 import events from './events'
 import filters from './filters'
+import favourites from './favourites'
 
 export default {
   config,
   events,
-  filters
+  filters,
+  favourites
 }
